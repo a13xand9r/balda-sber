374b855352357ba0f0df933da6cac95fb1fa02f3
Aleksandr <a13x.student@yandex.ru>
2021-12-22 11:28:36 +0300

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@sberdevices/plasma-ui'
+
+import { AppHeader } from './components/AppHeader'
+
+function App() {
+    return (
+        <Container>
+            <AppHeader />
+        </Container>
+    )
+}
+
+export default App
