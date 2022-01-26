6648581c27b7f1ac15bed34fc865a2ecb3dab0f5
Aleksandr <a13x.student@yandex.ru>
2022-01-26 13:17:12 +0300

    change rulePage ui

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -20,7 +20,7 @@ export const PageContainer = styled.div`
     margin-bottom: 8rem;
     text-align: center;
     /* width: 45rem; */
-    width: 90%;
+    width: 95%;
     @media (max-width: 700px){
         width: 90vw;
     }
